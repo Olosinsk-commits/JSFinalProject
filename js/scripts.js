f60a3da051e36303e5ae1f9b178cfe014bd2ca87
Noah <Noah.Greer@gmail.com>
2018-02-20 01:20:46 -0800

    Tidied HTML, and separated main styles and scripts from application specific ones.

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+/**
+ * @author Olga Osinskaya <ssinsk@yandex.ru>
+ * @author Noah Greer <Noah.Greer@gmail.com>
+ * @author Topher Gidos <tophgidos@gmail.com>
+ * 02/19/2018
+ * Winter 2018
+ * CSD 122 - JavaScript & jQuery
+ * Group Project - Group 3 (Olga Osinskaya, Noah Greer, and Topher Gidos)
+ *
+ * Main code for the group project that will be used on all pages.
+ */
---
+++ js/tic-tac-toe.js
@@ -0,0 +1,9 @@
+/**
+ * @author Olga Osinskaya <ssinsk@yandex.ru>
+ * 02/19/2018
+ * Winter 2018
+ * CSD 122 - JavaScript & jQuery
+ * Group Project - Group 3 (Olga Osinskaya, Noah Greer, and Topher Gidos)
+ *
+ * Code to play a game of Tic Tac Toe.
+ */
