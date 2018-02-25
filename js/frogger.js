e100db5edcad5a467d3c9f8a84f964e18c20d0db
tophgidos <36679442+tophgidos@users.noreply.github.com>
2018-02-25 10:39:34 -0800

    file path update
    updated image file paths

--- js/frogger.js
+++ js/frogger.js
@@ -5,41 +5,36 @@ var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d');
 	//variable for frog sprite
 var frog1 = new Image();
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-frog1.src = "images/frogger1.png";
+
+
+
+frog1.src = "img/frogger1.png";
 	//variable for cars
 var car = new Image();
-car.src = "images/cars.png";
-=======
->>>>>>> master
+car.src = "img/cars.png";
+
 frog1.src = "../img/frogger/frogger1.png";
 	//variable for cars
 var car = new Image();
 car.src = "../img/frogger/cars.png";
-<<<<<<< HEAD
 
-=======
->>>>>>> frogger.js
 //variables for first car
->>>>>>> master
+
 	//variables to allow all cars to move across the screen
 var carX1 = 100;
 var carSX1 = 0;
 		//variables for collision detection
 var carY1 = 400;
 //variables for second car
-<<<<<<< HEAD
+
 var carX2 = 500;
 var carSX2 = 60;
 var carY2 = 400;
 //
-=======
 var carX2 = 100;
 var carSX2 = 60;
 var carY2 = 400;
->>>>>>> master
+
 var carWidth = 60;
 var carHeight = 35;
 	//variables for drawImage Method
@@ -176,21 +171,6 @@ function drawCars() {
 				carSX2 = (Math.floor(Math.random() * 4)) * 60;
 		  }
 }
-<<<<<<< HEAD
-	//function that detects if the frog and car images are overlapping
-	function runOver () {
-		var carsX = [carX1, carX2];
-		var carsY = [carY1, carY2];
-		for (i = 0; i < carsX.length; i++) {
-			if (carsX[i] <= x + width &&
-				carsX[i] + carWidth >= x &&
-				carsY[i] + carHeight >= y &&
-				carsY[i] <= y + height) {
-					y = 488;
-			}
-		}
-	}
-=======
 	//funtion that detects if the frog and car images are overlapping
 	function runOver () {
 		var carsX = [carX1, carX2]; 
@@ -204,7 +184,6 @@ function drawCars() {
 		}
 	}
 	}
->>>>>>> master
 function draw() {
 	context.clearRect(0, 0, canvas.width, canvas.height); /*clears the previous iteration of the frog and redraws a new one.*/
 	drawBackground();
