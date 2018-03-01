cd7a4822ef7a8c55932042bfe345a86b332d071f
Noah <noah.greer@gmail.com>
2018-02-28 16:27:05 -0800

    Added accelleration, velocity, and direction vectors to the GameObject class. Added modification methods to the Vector2 class.

--- js/asteroids.js
+++ js/asteroids.js
@@ -8,78 +8,86 @@
  * Code to play a game of Asteroids.
  */
 
-
-
-var canvas;
-var ctx;
-
-var position = new Vector2(100, 100);
-var rotation = 0;
-
-var ship = new Image();
-ship.src = "../img/icons/png/steam_4096_black.png";
+var shipAvatar = new Image();
+shipAvatar.src = "../img/icons/png/steam_4096_black.png";
 
 var controls = {"up": false, "down": false, "left": false, "right": false, "space": false};
 
 $(document).ready(function() {
-  // Get reference to game canvas.
-  canvas = $("#gameCanvas").get(0);
-  // Check whether canvas is available in the browser.
-  if (canvas.getContext) {
-    ctx = canvas.getContext('2d');
-    requestAnimationFrame(update);
-  } else {
-    // Log that we could not get a reference to the context.
-    console.log("Could not get canvas context. Browser does not support HTML5 canvas.");
-  }
-
-  var gameManager = {
-      canvas : $("#gameCanvas").get(0),
-      start : function() {
-          this.context = this.canvas.getContext("2d");
-      },
-      clear : function() {
-          this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-      }
-  }
-
+    gameManager.start();
   }).keydown(function(e) {
     getInput(e);
   }).keyup(function(e) {
     getInput(e);
 });
 
-/*
-rotation formula:
-x = x * Math.cos(theta) - y * Math.sin(theta)
-y = x * Math.sin(theta) + y * Math.cos(theta)
-*/
+var gameManager = {
+    // Get reference to game canvas.
+    canvas : null,
+    start : function() {
+        this.canvas = $("#gameCanvas").get(0);
+        // Check whether canvas is available in the browser.
+        if (this.canvas.getContext) {
+          this.ctx = this.canvas.getContext('2d');
+          this.playerShip = new GameObject(new Vector2(10, 10), // position
+                                           new Vector2(100, 100), // scale
+                                           new Vector2(0, 0), // velocity
+                                           new Vector2(0, 0), // acceleration
+                                           new Vector2(0, 1), // direction
+                                           1); // collisionRadius
+          requestAnimationFrame(update);
+        } else {
+          // Log that we could not get a reference to the context.
+          console.log("Could not get canvas context. Browser does not support HTML5 canvas.");
+        }
+    }
+}
 
 function update() {
   updateMovement();
+  gameManager.playerShip.update();
   clearCanvas();
   drawBackground();
   drawPlayerShip();
+  drawHealth();
+  drawScore();
   requestAnimationFrame(update);
 }
 
 function clearCanvas() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  gameManager.ctx.clearRect(0, 0, gameManager.canvas.width, gameManager.canvas.height);
 }
 
 function drawBackground() {
-  ctx.fillStyle="#00BBFF";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  gameManager.ctx.fillStyle="#00BBFF";
+  gameManager.ctx.fillRect(0, 0, gameManager.canvas.width, gameManager.canvas.height);
 }
 
-var size = new Vector2(100, 100);
+function drawHealth() {
+  gameManager.ctx.font = "30px Arial";
+  gameManager.ctx.fillStyle = "#000000";
+  gameManager.ctx.textAlign = "left";
+  let health = 100;
+  let healthText = "Health: " + health;
+  gameManager.ctx.fillText(healthText, 2, 30);
+}
+
+function drawScore() {
+  gameManager.ctx.font = "30px Arial";
+  gameManager.ctx.fillStyle = "#000000";
+  gameManager.ctx.textAlign = "right";
+  let score = 100;
+  let scoreText = "Score: " + score;
+  gameManager.ctx.fillText(scoreText, gameManager.canvas.width - 2, 30);
+}
 
 function drawPlayerShip() {
-  ctx.save();
-  ctx.translate(position.x, position.y);
-  ctx.rotate(rotation * (Math.PI / 180));
-  ctx.drawImage(ship, -size.x/2, -size.y/2, size.x, size.y);
-  ctx.restore();
+  gameManager.ctx.save();
+  gameManager.ctx.translate(gameManager.playerShip.position.x, gameManager.playerShip.position.y);
+  let angle = Vector2.angleBetween(Vector2.down, gameManager.playerShip.direction);
+  gameManager.ctx.rotate(angle);
+  gameManager.ctx.drawImage(shipAvatar, -gameManager.playerShip.scale.x/2, -gameManager.playerShip.scale.y/2, gameManager.playerShip.scale.x, gameManager.playerShip.scale.y);
+  gameManager.ctx.restore();
 }
 
 function getInput(e) {
@@ -108,18 +116,19 @@ function getInput(e) {
 }
 
 function updateMovement() {
-  let speed = 5;
+  let acceleration = 2;
+  let rotationAngle = 2 * (Math.PI / 180);
   if (controls.up) {
-    position.y -= speed;
+    gameManager.playerShip.acceleration.add(gameManager.playerShip.direction);
   }
   if (controls.down) {
-    position.y += speed;
+    // do nothing.
   }
   if (controls.left) {
-    rotation -= speed;
+    gameManager.playerShip.direction.rotate(-rotationAngle);
   }
   if (controls.right) {
-    rotation += speed;
+    gameManager.playerShip.direction.rotate(rotationAngle);
   }
 }
 
@@ -134,16 +143,12 @@ function Vector2(x, y) {
   // *** BEGIN INPUT VALIDATION ***
   // If no input was received for the 'x' parameter.
   if (x === undefined) throw "The 'x' parameter is required!";
-  // If the 'name' parameter is not a number.
+  // If the 'x' parameter is not a number.
   if (typeof x !== 'number') throw "The 'x' parameter must be a number.";
-  // If the 'x' parameter is less than zero.
-  if (x < 0) throw "The 'x' parameter must be greater than or equal to zero.";
-  // If no input was received for the 'x' parameter.
+  // If no input was received for the 'y' parameter.
   if (y === undefined) throw "The 'y' parameter is required!";
   // If the 'y' parameter is not a number.
   if (typeof y !== 'number') throw "The 'y' parameter must be a number.";
-  // If the 'y' parameter is less than zero.
-  if (x < 0) throw "The 'x' parameter must be greater than or equal to zero.";
   // *** END INPUT VALIDATION ***
 
   // Set the new GameObject's properties.
@@ -155,28 +160,168 @@ function Vector2(x, y) {
                                   ", y: " + this.y + ")");
 }
 
+Vector2.down = new Vector2(0, -1);
+Vector2.left = new Vector2(-1, 0);
+Vector2.right = new Vector2(1, 0);
+Vector2.up = new Vector2(0, 1);
+
+Vector2.add = function(v1, v2) {
+  return new Vector2(v1.x + v2.x, v1.y + v2.y);
+}
+
+Vector2.angleBetween = function (fromV, toV) {
+  // *** BEGIN INPUT VALIDATION ***
+  // If no input was received for the 'fromV' parameter.
+  if (fromV === undefined) throw "The 'fromV' parameter is required!";
+  // If the 'fromV' parameter is not a Vector2.
+  if (!fromV instanceof Vector2) throw "The 'fromV' parameter must be a Vector2 object.";
+  // If no input was received for the 'toV' parameter.
+  if (toV === undefined) throw "The 'toV' parameter is required!";
+  // If the 'toV' parameter is not a Vector2.
+  if (!toV instanceof Vector2) throw "The 'toV' parameter must be a Vector2 object.";
+  // *** END INPUT VALIDATION ***
+
+  let dotProduct = fromV.x * toV.x + fromV.y * toV.y;
+  let length = fromV.magnitude() * toV.magnitude();
+  let angle = Math.acos(dotProduct/length);
+  return angle;
+}
+
+// Adds another vector to this vector.
+Vector2.prototype.add = function (otherVector2) {
+  // *** BEGIN INPUT VALIDATION ***
+  // If no input was received for the 'position' parameter.
+  if (otherVector2 === undefined) throw "The 'otherVector2' parameter is required!";
+  // If the 'otherVector2' parameter is not a Vector2.
+  if (!otherVector2 instanceof Vector2) throw "The 'otherVector2' parameter must be a Vector2 object.";
+  // *** END INPUT VALIDATION ***
+
+  this.x = this.x + otherVector2.x;
+  this.y = this.y + otherVector2.y;
+}
+
+// Subtracts another vector from this vector.
+Vector2.prototype.subtract = function (otherVector2) {
+  // *** BEGIN INPUT VALIDATION ***
+  // If no input was received for the 'position' parameter.
+  if (otherVector2 === undefined) throw "The 'otherVector2' parameter is required!";
+  // If the 'otherVector2' parameter is not a Vector2.
+  if (!otherVector2 instanceof Vector2) throw "The 'otherVector2' parameter must be a Vector2 object.";
+  // *** END INPUT VALIDATION ***
+
+  this.x = this.x - otherVector2.x;
+  this.y = this.y - otherVector2.y;
+}
+
 /**
- * Creates an instance of a GameObject with a position vector, velocity vector, and rotation vector.
+ * Multiplies this vector's components by a scalar value.
+ * @param {number}
+ */
+Vector2.prototype.multiply = function (scalar) {
+  // *** BEGIN INPUT VALIDATION ***
+  // If no input was received for the 'scalar' parameter.
+  if (scalar === undefined) throw "The 'scalar' parameter is required!";
+  // If the 'scalar' parameter is not a number.
+  if (typeof scalar !== 'number') throw "The 'scalar' parameter must be a number.";
+  // *** END INPUT VALIDATION ***
+
+  this.x *= scalar;
+  this.y *= scalar;
+}
+
+/**
+ * Divides this vector's components by a scalar value.
+ * @param {number}
+ */
+Vector2.prototype.divide = function (scalar) {
+  // *** BEGIN INPUT VALIDATION ***
+  // If no input was received for the 'scalar' parameter.
+  if (scalar === undefined) throw "The 'scalar' parameter is required!";
+  // If the 'scalar' parameter is not a number.
+  if (typeof scalar !== 'number') throw "The 'scalar' parameter must be a number.";
+  // *** END INPUT VALIDATION ***
+
+  this.x /= scalar;
+  this.y /= scalar;
+}
+
+/**
+ * Rotates this vector by an angle value.
+ * @param {number}
+ */
+Vector2.prototype.rotate = function (angle) {
+  // *** BEGIN INPUT VALIDATION ***
+  // If no input was received for the 'angle' parameter.
+  if (angle === undefined) throw "The 'angle' parameter is required!";
+  // If the 'angle' parameter is not a number.
+  if (typeof angle !== 'number') throw "The 'angle' parameter must be a number.";
+  // *** END INPUT VALIDATION ***
+
+  let x = this.x;
+  let y = this.y;
+  this.x = x * Math.cos(angle) - y * Math.sin(angle);
+  this.y = x * Math.sin(angle) + y * Math.cos(angle);
+}
+
+/**
+ * @return {number} the magnitude of the vector.
+ */
+Vector2.prototype.magnitude = function () {
+  // Return the magnitude of the vector calculated using the pythagorean theorem.
+  return Math.sqrt(this.x*this.x + this.y*this.y);
+}
+
+Vector2.prototype.normalize = function () {
+  let m = this.magnitude();
+  if (m > 0) {
+    this.divide(m);
+  }
+}
+
+/**
+ * Clamps the magnitude of the vector.
+ * @param {number}
+ */
+Vector2.prototype.clampMagnitude = function (newMagnitude) {
+  if (this.magnitude() > newMagnitude) {
+    this.normalize();
+    this.multiply(newMagnitude);
+  }
+}
+
+/**
+ * Creates an instance of a GameObject.
  * @constructor
  * @param {Vector2} position The GameObject's position vector.
+ * @param {Vector2} scale The GameObject's scale vector.
  * @param {Vector2} velocity The GameObject's velocity vector.
- * @param {Vector2} rotation The GameObject's rotation vector.
+ * @param {Vector2} acceleration The GameObject's acceleration vector.
+ * @param {Vector2} direction The GameObject's direction vector.
+ * @param {number} collisionRadius The GameObject's collisionRadius.
  * @return {GameObject} The new GameObject object.
  */
-function GameObject(position, velocity, rotation, collisionRadius) {
+function GameObject(position, scale, velocity, acceleration, direction, collisionRadius) {
   // *** BEGIN INPUT VALIDATION ***
   // If no input was received for the 'position' parameter.
   if (position === undefined) throw "The 'position' parameter is required!";
   // If the 'position' parameter is not a Vector2.
-  if (position instanceof Vector2) throw "The 'position' parameter must be a Vector2 object.";
+  if (!position instanceof Vector2) throw "The 'position' parameter must be a Vector2 object.";
+  // If no input was received for the 'scale' parameter.
+  if (scale === undefined) throw "The 'scale' parameter is required!";
+  // If the 'scale' parameter is not a Vector2.
+  if (!scale instanceof Vector2) throw "The 'scale' parameter must be a Vector2 object.";
   // If no input was received for the 'velocity' parameter.
   if (velocity === undefined) throw "The 'velocity' parameter is required!";
   // If the 'velocity' parameter is not a Vector2.
-  if (velocity instanceof Vector2) throw "The 'velocity' parameter must be a Vector2 object.";
-  // If no input was received for the 'rotation' parameter.
-  if (rotation === undefined) throw "The 'rotation' parameter is required!";
-  // If the 'rotation' parameter is not a Vector2.
-  if (rotation instanceof Vector2) throw "The 'rotation' parameter must be a Vector2 object.";
+  if (!velocity instanceof Vector2) throw "The 'velocity' parameter must be a Vector2 object.";
+  // If no input was received for the 'acceleration' parameter.
+  if (acceleration === undefined) throw "The 'acceleration' parameter is required!";
+  // If the 'acceleration' parameter is not a Vector2.
+  if (!acceleration instanceof Vector2) throw "The 'acceleration' parameter must be a Vector2 object.";
+  // If no input was received for the 'direction' parameter.
+  if (direction === undefined) throw "The 'direction' parameter is required!";
+  // If the 'direction' parameter is not a Vector2.
+  if (!direction instanceof Vector2) throw "The 'direction' parameter must be a Vector2 object.";
   // If no input was received for the 'collisionRadius' parameter.
   if (collisionRadius === undefined) throw "The 'collisionRadius' parameter is required!";
   // If the 'collisionRadius' parameter is not a number.
@@ -187,47 +332,23 @@ function GameObject(position, velocity, rotation, collisionRadius) {
 
   // Set the new GameObject's properties.
   this.position = position;
+  this.scale = scale;
   this.velocity = velocity;
-  this.rotation = rotation;
+  this.acceleration = acceleration;
+  this.direction = direction;
   this.collisionRadius = collisionRadius;
 
   // Log that the new GameObject was created sucessfully.
   console.log("Created new GameObject: position: " + this.position +
+                                       ", scale: " + this.scale +
                                     ", velocity: " + this.velocity +
-                                    ", rotation: " + this.rotation +
+                                ", acceleration: " + this.acceleration +
+                                    ", direction: " + this.direction +
                              ", collisionRadius: " + this.collisionRadius);
 }
 
-/**
- * Creates an instance of an Asteroid with a position vector, velocity vector, and rotation vector.
- * @constructor
- * @param {Vector2} position The Asteroid's position vector.
- * @param {Vector2} velocity The Asteroid's velocity vector.
- * @param {Vector2} rotation The Asteroid's rotation vector.
- * @return {Asteroid} The new Asteroid object.
- */
-function Asteroid(position, velocity, rotation, collisionRadius, health) {
-  try {
-    GameObject.call(this, position, velocity, rotation, collisionRadius);
-  } catch (e) {
-    console.log(e.message);
-  }
-  // *** BEGIN INPUT VALIDATION ***
-  // If no input was received for the 'position' parameter.
-  if (position === undefined) throw "The 'position' parameter is required!";
-  // If the 'position' parameter is not a Vector2.
-  if (position instanceof Vector2) throw "The 'position' parameter must be a Vector2 object.";
-  // *** END INPUT VALIDATION ***
-
-  // Set the new Asteroid's properties.
-  this.position = position;
-  this.velocity = velocity;
-  this.rotation = rotation;
-  this.collisionRadius = collisionRadius;
-
-  // Log that the new Asteroid was created sucessfully.
-  console.log("Created new Asteroid: position: " + this.position +
-                                    ", velocity: " + this.velocity +
-                                    ", rotation: " + this.rotation +
-                             ", collisionRadius: " + this.collisionRadius);
-}
+GameObject.prototype.update = function () {
+  this.velocity.add(this.acceleration);
+  this.velocity.clampMagnitude(5);
+  this.position.add(this.velocity);
+};
